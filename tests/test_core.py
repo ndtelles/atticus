11c5ee5ea66142e7a1b18ae97d3d378ddb1b9c97
Nathan Telles <me@nathantelles.com>
2020-01-21 00:38:51 -0800

    Fixed tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -36,7 +36,7 @@ class TestAtticusAPI:
 
     def test_load(self, empty_atticus):
         empty_atticus.load('test', './tests/test_configs/simple_tcp.yaml')
-        assert 'test' in empty_atticus._mb_processes
+        assert 'test' in empty_atticus._mb_procs
 
     def test_load_already_loaded(self, empty_atticus):
         empty_atticus.load('test', './tests/test_configs/simple_tcp.yaml')
@@ -44,26 +44,26 @@ class TestAtticusAPI:
             empty_atticus.load('test', './tests/test_configs/simple_tcp.yaml')
 
     def test_unload(self, loaded_atticus):
-        mb_name = next(iter(loaded_atticus._mb_processes))
+        mb_name = next(iter(loaded_atticus._mb_procs))
         loaded_atticus.unload(mb_name)
-        assert mb_name not in loaded_atticus._mb_processes
+        assert mb_name not in loaded_atticus._mb_procs
 
     def test_unload_not_loaded(self, empty_atticus):
         with pytest.raises(MockingbirdNotFound):
             empty_atticus.unload('literally_anything')
 
     def test_unload_running(self, running_atticus):
-        mb_name = next(iter(running_atticus._mb_processes))
+        mb_name = next(iter(running_atticus._mb_procs))
         with pytest.raises(MockingbirdRunning):
             running_atticus.unload(mb_name)
 
     def test_start(self, loaded_atticus):
-        mb_name = next(iter(loaded_atticus._mb_processes))
+        mb_name = next(iter(loaded_atticus._mb_procs))
         loaded_atticus.start(mb_name)
-        assert loaded_atticus._mb_processes[mb_name].status is MockingbirdProcess.Status.RUNNING
+        assert loaded_atticus._mb_procs[mb_name].status is MockingbirdProcess.Status.RUNNING
 
     def test_start_already_started(self, running_atticus):
-        mb_name = next(iter(running_atticus._mb_processes))
+        mb_name = next(iter(running_atticus._mb_procs))
         with pytest.raises(MockingbirdRunning):
             running_atticus.start(mb_name)
 
@@ -72,16 +72,16 @@ class TestAtticusAPI:
             empty_atticus.start('literally_anything')
 
     def test_stop(self, running_atticus):
-        mb_name = next(iter(running_atticus._mb_processes))
+        mb_name = next(iter(running_atticus._mb_procs))
         running_atticus.stop(mb_name)
-        assert running_atticus._mb_processes[mb_name].status is MockingbirdProcess.Status.STOPPED
+        assert running_atticus._mb_procs[mb_name].status is MockingbirdProcess.Status.STOPPED
 
     def test_stop_not_loaded(self, empty_atticus):
         with pytest.raises(MockingbirdNotFound):
             empty_atticus.stop('literally_anything')
 
     def test_stop_not_running(self, loaded_atticus):
-        mb_name = next(iter(loaded_atticus._mb_processes))
+        mb_name = next(iter(loaded_atticus._mb_procs))
         with pytest.raises(MockingbirdNotRunning):
             loaded_atticus.stop(mb_name)
 
@@ -93,5 +93,5 @@ class TestAtticusAPI:
         empty_atticus.start(mb2)
         empty_atticus.stop_all()
 
-        assert empty_atticus._mb_processes[mb1].status is MockingbirdProcess.Status.STOPPED
-        assert empty_atticus._mb_processes[mb2].status is MockingbirdProcess.Status.STOPPED
+        assert empty_atticus._mb_procs[mb1].status is MockingbirdProcess.Status.STOPPED
+        assert empty_atticus._mb_procs[mb2].status is MockingbirdProcess.Status.STOPPED
